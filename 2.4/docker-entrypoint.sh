ff219be6f7b5115ffd2a8117c107dd94c3c16273
Hoelee <me@hoelee.com>
2022-04-17 02:07:53 +0800

    Update docker-entrypoint.sh

--- 2.4/docker-entrypoint.sh
+++ 2.4/docker-entrypoint.sh
@@ -101,6 +101,8 @@ fi
 # Create directories for Dav data and lock database.
 [ ! -d "/var/lib/dav/data" ] && mkdir -p "/var/lib/dav/data"
 [ ! -e "/var/lib/dav/DavLock" ] && touch "/var/lib/dav/DavLock"
-chown -R www-data:www-data "/var/lib/dav"
+
+# Hoelee Remove file permission change
+# chown -R www-data:www-data "/var/lib/dav"
 
 exec "$@"
